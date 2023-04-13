a8a7128f106d211f78c83ef9796bf870d72c1285
Chloe Hausser <chloerae018@gmail.com>
2023-04-12 23:05:24 -0400

    basic cli flag processing

--- src/bitcrusher.nim
+++ src/bitcrusher.nim
@@ -1,6 +1,10 @@
-import os
+import os, strutils
 
 const FLAGS = ["-a", "-A", "-o", "-O", "-r", "-R", "-d", "-D", "-h", "-H"]
+
+type 
+    ProgOpt = enum
+        optNone, optDebug, optAssemble, optExecute
 
 proc usage() = 
     stdout.write("-- Bitcrusher CPU Emulator --\n")
@@ -8,7 +12,7 @@ proc usage() =
     stdout.write("    -a: assemble source file to bytecode\n")
     stdout.write("    -o: specify the outputfile\n")
     stdout.write("    -r: execute the given bytecode file (must assemble file first if not provided bytecode)\n")
-    stdout.write("    -d: run in debug mode (used for testing)")
+    stdout.write("    -d: run in debug mode (used for testing)\n")
     stdout.write("    -h: print this help message\n")
 
 proc assembleFile(infile: string, outfile: string) =
@@ -21,7 +25,60 @@ proc runDebug(infile: string, readfile: bool) =
     echo "TODO: runDebug"
 
 proc processFlags(args: openArray[string]) =
-    echo "todo processFlags"
+    var
+        infile  = ""
+        outfile = ""
+        opt: ProgOpt
+        currflag = ""
+
+    for i, a in args:
+        if a.startsWith('-'):
+            if a in FLAGS:
+                if a == "-h" or a == "-H":
+                    usage()
+                    break
+                elif a == "-d" or a == "-D":
+                    opt = optDebug
+                    currflag = "-d" 
+                else:
+                    currflag = a
+            else:
+                echo "ERROR: Unknown flag: " & a & "\n    use `-h` for a list of supported flags"
+                quit(1)
+        else:
+            if currflag != "":
+                case currflag:
+                    of "-a", "-A":
+                        opt = optAssemble
+                        infile = a
+                        currflag = ""
+                    of "-o", "-O":
+                        outfile = a
+                        currflag = ""
+                    of "-r", "-R":
+                        opt = optExecute
+                        infile = a
+                        currflag = ""
+                    of "-d", "-D":
+                        opt = optDebug
+                        infile = a
+                        currflag = ""
+                    else:
+                        echo "ERROR: Unrecognized flag: [" & currflag & "]"
+                        break
+
+    case opt:
+        of optDebug:
+            if infile == "":
+                runDebug(infile, false)
+            else:
+                runDebug(infile, true)
+        of optAssemble:
+            assembleFile(infile, outfile)
+        of optExecute:
+            executeFile(infile)
+        else:
+            echo "infile: " & infile & " outfile: " & outfile & " option: " & $opt
 
 proc main() =
     let args = commandLineParams()
